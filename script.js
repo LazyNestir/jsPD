04c579a8f1aa99fea6fca975c7be36777c9a007a
LazyNestir <90598654+LazyNestir@users.noreply.github.com>
2022-03-22 14:01:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-alert('Салам')
+let title = "jspd";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 35500;
+let rollback = Math.round(Math.random() * 30);
+let fullPrice = 233000;
+let adaptive = true;
+let mas = screens.split('');
+
+console.log("salam")
+
+console.log('название проекта:' + " " + title)
+console.log('Стоимость разработки сайта:' + " " + fullPrice + " " + 'Рублей')
+console.log('Стоимость верстки экранов:' + " " + screenPrice + " " + 'Рублей')
+console.log('Адаптация:' + " " + adaptive)
+console.log('Экраны:' + " " + screens.length)
+console.log(screens.toLowerCase())
+console.log(mas)
+console.log('rollback:' + " " + rollback)
+console.log('Процент отката посреднику за работу' + " " + fullPrice * (rollback/100))
+
+console.log(typeof screens)
+console.log(typeof screenPrice)
+console.log(typeof rollback)
+console.log(typeof fullPrice)
+console.log(typeof adaptive)

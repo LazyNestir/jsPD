93e65511c0bb7a566fe56fcc1e1deef2806e41a7
LazyNestir <90598654+LazyNestir@users.noreply.github.com>
2022-04-07 11:32:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
-'use strict'
+'use strict';
 
-const books  = document.querySelectorAll('.books')
-let book = document.querySelectorAll('.book')
-const removeAdv = document.querySelector('.adv').remove()
-const title = document.querySelectorAll('li')
-const createNewLi = document.createElement('li');
-
-
-
-document.querySelector('body').style.backgroundImage = 'url(./image/you-dont-know-js.jpg)'
+const btn = document.getElementById('btn')
+let square = document.getElementById('square')
+let input = document.getElementById('text')
+let e_btn = document.getElementById('e_btn')
+let range = document.getElementById('range')
+let circle = document.getElementById('circle')
+let span = document.getElementById('range-span')
 
 
-book[1].after(book[0])
-book[0].after(book[4])
-book[4].after(book[3])
-book[3].after(book[5])
+const color = function () {
+        square.style.backgroundColor = input.value  
+}
 
+btn.addEventListener('click', () => {
+    color()
+})
 
-console.log(book);
+const invisible = function () {
+    e_btn.style.display = 'none'
+}
 
-book[4].querySelector('a').text = 'Книга 3. this и Прототипы Объектов'
+e_btn.addEventListener('click', () => {
+    invisible()
+})
 
+const target = function (event) {
+    span.textContent = event.target.value
+}
 
-console.log(title);
+range.addEventListener('change', target)
 
-title[3].after(title[6])
-title[6].after(title[8])
-title[9].after(title[2])
-title[47].after(title[55])
-title[55].after(title[49])
-title[49].after(title[50])
-title[53].after(title[51])
+const hw = function () {
+    circle.style.height = range.value + '%'
+    circle.style.width = range.value + '%'
+}
 
+range.addEventListener('input', hw)
 
-createNewLi.textContent = 'Глава 8: За пределами ES6'
-book[2].querySelector('ul').append(createNewLi);
-createNewLi.after(title[9])
-
-createNewLi.after(title[26])
+console.dir(circle)

66c070a5f89026ad8c5678948edb9ce956bc5318
LazyNestir <90598654+LazyNestir@users.noreply.github.com>
2022-04-28 23:48:33 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
+import calculator from './modules/calculator'
+
 
 
 timer('30 april 2022')
 menu()
 modal()
+calculator()
